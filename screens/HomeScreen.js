6bf34c198d99c2ecb60e003cf59e46ee1b34bb9f
Nirpendra Chaudhary <55339698+Nirpendra09@users.noreply.github.com>
2022-07-30 16:58:15 +0000

    Home Screen Image and Icon setup

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon
+} from "react-native-heroicons/outline"
 
 const HomeScreen = () => {
-  return (
-    <View>
-      <Text className="text-red-500">HomeScreen</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation();
 
-export default HomeScreen
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView className="pt-5">
+      <Text className="text-red-500">
+        {/* Header  */}
+        <View className="flex-row pb-3 item-center mx-4 space-x-2">
+          <Image
+            source={{
+              uri: "http://links.papareact.com/wru",
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+
+          <View>
+            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+            <Text className="font-bold text-xl">Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+            </Text>
+          </View>
+
+        </View>
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;

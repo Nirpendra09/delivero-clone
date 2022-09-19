7482c49ba8bd35fddc0b0b44f0ed673244645c7f
Nirpendra Chaudhary <55339698+Nirpendra09@users.noreply.github.com>
2022-09-19 16:58:51 +0000

    setting up redux and Basket functionality

--- App.js
+++ App.js
@@ -7,20 +7,26 @@ import HomeScreen from "./screens/HomeScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import BasketScreen from "./screens/BasketScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/* <Provider store={store}> */}
+      <Provider store={store}>
         <TailwindProvider>
           <Stack.Navigator>
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+            <Stack.Screen
+              name="Basket"
+              component={BasketScreen}
+              options={{ presentation: "modal", headerShown: false }}
+            />
           </Stack.Navigator>
         </TailwindProvider>
-      {/* </Provider> */}
+      </Provider>
     </NavigationContainer>
   );
 }
